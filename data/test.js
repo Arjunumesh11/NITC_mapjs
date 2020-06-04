31fb9b1e2f4b68272b12d0dda0cf7c8920256f99
arjun <arjunumesh11@gmail.com>
2020-06-04 17:11:06 +0530

    bugs

--- data/test.js
+++ data/test.js
@@ -1,6 +1,23 @@
 var Datastore = require('nedb');
 db = new Datastore('nitc_nodes_all.db');
+db1 = new Datastore('nitc_paths_all.db');
+nit = new Datastore('nitc_paths.db');
 db.loadDatabase();
-db.find({ id: 3655147185 }, (err, doc) => {
-    console.log(doc);
+db1.loadDatabase();
+nit.loadDatabase();
+nit.find({}, (err, doc) => {
+    for (key in doc) {
+        for (node in doc[key].nodes) {
+            db.findOne({ id: doc[key].nodes[node] }, (err, latlng) => {
+                console.log(doc[key].id);
+                let ids = doc[key].id;
+                if (latlng) {
+                    db1.find({ id: ids }, (err, de) => {
+
+                    });
+                }
+            });
+        }
+    }
 });
+db1.persistence.compactDatafile();
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const Datastore = require('nedb');
 var nitc_buliding = new Datastore('./data/nitc_buliding.db');
+var nitc_paths = new Datastore('./data/nitc_nodes.db');
+var nitc_nodes = new Datastore('./data/nitc_nodes_all.db');
 nitc_buliding.loadDatabase();
+nitc_paths.loadDatabase();
+nitc_nodes.loadDatabase();
 
 const routes = express();
 routes.get('/api/node/:name', (req, res) => {
@@ -18,5 +22,28 @@ routes.get('/api/node/:name', (req, res) => {
             res.send(doc);
         });
     }
+});
+routes.get('/api/path/:name', (req, res) => {
+    var data = req.params;
+    var paths = [];
+    // if (data.name == "all") {
+    //     nitc_paths.find({ "tags.highway": { $exists: true, $nin: ['unclassified', 'residential'] } }, (err, doc) => {
+    //         res.send(doc);
+    //     });
+    // }
+    // else {
+    nitc_paths.findOne({ "tags.highway": { $exists: true, $nin: ['unclassified', 'residential'] } }, (err, doc) => {
+        for (key in doc.nodes) {
+            nitc_nodes.findOne({ id: doc.nodes[key] }, (err, nodes_doc) => {
+                res.send(nodes_doc);
+            });
+
+        }
+        // res.send(doc);
+        //console.log(paths);
+    });
+    //     });
+    // }
+    // console.log("sa");
 });
 module.exports = routes;
